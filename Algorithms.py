0afc38a6f4801b6517ac320dd84c5b27e9e80580
TowKnee0 <Tonyyqhe@gmail.com>
2020-11-28 15:28:19 -0500

    Added initial files

---
+++ Algorithms.py
@@ -0,0 +1,134 @@
+import math
+import numpy as np
+
+class HoughCluster:
+    """Clasterize and merge each cluster of cv2.HoughLinesP() output
+
+    This was taken from https://stackoverflow.com/questions/45531074/how-to-merge-lines-after-houghlinesp
+    and modified.
+
+    >>> a = HoughCluster()
+    >>> foo = a.process_lines(houghP_lines, binary_image)
+    """
+
+    def get_orientation(self, line):
+        # Get orientation of a line, using its length https://en.wikipedia.org/wiki/Atan2
+        orientation = math.atan2(abs((line[0] - line[2])), abs((line[1] - line[3])))
+        return math.degrees(orientation)
+
+    def checker(self, line_new, groups, min_distance_to_merge, min_angle_to_merge):
+        # Check if line have enough distance and angle to be count as similar
+        for group in groups:
+            # walk through existing line groups
+            for line_old in group:
+                # check distance
+                if self.get_distance(line_old, line_new) < min_distance_to_merge:
+                    # check the angle between lines
+                    orientation_new = self.get_orientation(line_new)
+                    orientation_old = self.get_orientation(line_old)
+                    # if all is ok -- line is similar to others in group
+                    if abs(orientation_new - orientation_old) < min_angle_to_merge:
+                        group.append(line_new)
+                        return False
+        # if it is totally different line
+        return True
+
+    def distance_to_line(self, point, line):
+        """Get distance between point and line
+        https://stackoverflow.com/questions/40970478/python-3-5-2-distance-from-a-point-to-a-line
+        """
+        px, py = point
+        x1, y1, x2, y2 = line
+        x_diff = x2 - x1
+        y_diff = y2 - y1
+        num = abs(y_diff * px - x_diff * py + x2 * y1 - y2 * x1)
+        den = math.sqrt(y_diff ** 2 + x_diff ** 2)
+        return num / den
+
+
+    def get_distance(self, a_line, b_line):
+        """Get all possible distances between each dot of two lines and second line
+        return the shortest
+        """
+        dist1 = self.distance_to_line(a_line[:2], b_line)
+        dist2 = self.distance_to_line(a_line[2:], b_line)
+        dist3 = self.distance_to_line(b_line[:2], a_line)
+        dist4 = self.distance_to_line(b_line[2:], a_line)
+
+        return min(dist1, dist2, dist3, dist4)
+
+    def merge_lines_pipeline_2(self, lines):
+        #Clusterize (group) lines
+        groups = []  # all lines groups are here
+        # Parameters to play with
+        min_distance_to_merge = 30
+        min_angle_to_merge = 30
+        # first line will create new group every time
+        groups.append([lines[0]])
+        # if line is different from existing gropus, create a new group
+        for line_new in lines[1:]:
+            if self.checker(line_new, groups, min_distance_to_merge, min_angle_to_merge):
+                groups.append([line_new])
+
+        return groups
+
+    def merge_lines_segments1(self, lines):
+        """Sort lines cluster and return first and last coordinates
+        """
+        orientation = self.get_orientation(lines[0])
+
+        # special case
+        if (len(lines) == 1):
+            return [lines[0][:2], lines[0][2:]]
+
+        # [[1,2,3,4],[]] to [[1,2],[3,4],[],[]]
+        points = []
+        for line in lines:
+            points.append(line[:2])
+            points.append(line[2:])
+        # if vertical
+        if orientation < 90:
+            # sort by y
+            points = sorted(points, key=lambda point: point[1])
+        else:
+            # sort by x
+            points = sorted(points, key=lambda point: point[0])
+
+        # return first and last point in sorted group
+        # [[x,y],[x,y]]
+        return [points[0], points[-1]]
+
+    def process_lines(self, lines, img):
+        """Main function for lines from cv.HoughLinesP() output merging.
+        """
+        lines_x = []
+        lines_y = []
+        # for every line of cv2.HoughLinesP()
+        for line_i in [l[0] for l in lines]:
+            orientation = self.get_orientation(line_i)
+            # if vertical
+
+            if orientation < 90:
+                lines_y.append(line_i)
+            else:
+                lines_x.append(line_i)
+
+        lines_y = sorted(lines_y, key=lambda line: line[1])
+        lines_x = sorted(lines_x, key=lambda line: line[0])
+        merged_lines_all = []
+
+        # for each cluster in vertical and horizantal lines leave only one line
+        for i in [lines_x, lines_y]:
+            if len(i) > 0:
+                groups = self.merge_lines_pipeline_2(i)
+                merged_lines = []
+                for group in groups:
+                    merged_lines.append(self.merge_lines_segments1(group))
+
+                merged_lines_all.extend(merged_lines)
+
+        return np.array(merged_lines_all).reshape((-1, 4))
+
+
+
+
---
+++ main.py
@@ -0,0 +1,95 @@
+import cv2
+import numpy as np
+from PIL import ImageGrab
+import Algorithms
+import Driving_Algorithms
+
+def mask_region(screen, vertices):
+    """Returns region of interest based on given vertices.
+
+    Note: screen should be processed already using Canny edge detection.
+    """
+
+    # Creates a mask of 0's. The pixels in the region of interest are filled
+    # to white. Finally bitwise_and returns white when both mask and screen are
+    # white (per pixel) and black otherwise.
+    mask = np.zeros_like(screen)
+    cv2.fillPoly(mask, vertices, 255)
+    np.set_printoptions(threshold=np.inf)
+
+    return cv2.bitwise_and(screen, mask)
+
+
+def lane_lines(screen):
+    """Takes in a screen as input and performs a hough transform on the image. The hough
+    lines are then drawn on top of the input image.
+
+    Note: screen should be at least edge detection processed. Better if screen is passed
+          after region of interest is applied to edge detection.
+    """
+    lines_mess = cv2.HoughLinesP(screen, rho=1, theta=np.pi/180, threshold=180, minLineLength=60, maxLineGap=5)
+
+    # Note: lines is in the form [[[here]], [[here]]...]
+
+    if lines_mess is not None:
+        sorter = Algorithms.HoughCluster()
+        lines = sorter.process_lines(lines_mess, screen)
+        temp = lines
+
+        # only return 2 lines
+        if len(lines) > 2:
+            temp = []
+            max_slope = 0.1
+            min_slope = - 0.1
+            for line in lines:
+                slope = (line[3] - line[1]) / (line[2] - line[0])
+                if slope > max_slope:
+                    max_slope = slope
+                    if len(temp) == 2:
+                        temp.pop()
+                    temp.append(line)
+                elif slope < min_slope:
+                    min_slope = slope
+                    if len(temp) == 2:
+                        temp.pop(0)
+                    temp.insert(0, line)
+
+        return temp
+
+
+def process_image(screen):
+    """ Takes in the screenshot, applies edge detection and mask and blur
+    then returns the new screen.
+    """
+    gray_screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray_screen, threshold1=200, threshold2=300)
+
+    vertices = np.array([[10, 450], [400, 375], [650, 375], [950, 450], [950, 700], [750, 700], [600, 450], [400, 450], [250, 700], [10, 700]], dtype=np.int32)
+
+    masked = mask_region(edges, [vertices])
+    blur = cv2.GaussianBlur(masked, (3, 3), 0)
+
+    # lanes = lane_lines(blur)
+    #
+    # if lanes is not None:
+    #     if len(lanes) == 2:
+    #         Driving_Algorithms.drive_max_dist(lanes)
+    #
+    #     for line in lanes:
+    #         cv2.line(blur, (line[0], line[1]), (line[2], line[3]), (255, 0, 255), 10)
+
+    return blur
+
+
+while True:
+    # store raw pixel data into np array
+    screen = np.array(ImageGrab.grab(bbox=(0, 100, 950, 800)))
+
+    processed = process_image(screen)
+    lanes = lane_lines(processed)
+    if lanes is not None:
+        Driving_Algorithms.drive_max_dist(lanes, (950, 800))
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
